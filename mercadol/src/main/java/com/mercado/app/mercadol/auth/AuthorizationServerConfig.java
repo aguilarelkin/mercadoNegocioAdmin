435a9cd3347211d55c36f826d90f82caab5458f1
Elkin Aguilar <aguilar.elkin99@gmail.com>
2023-08-09 08:25:48 -0500

    Rename and change folders

--- mercadol/src/main/java/com/mercado/app/mercadol/auth/AuthorizationServerConfig.java
+++ mercadol/src/main/java/com/mercado/app/mercadol/auth/AuthorizationServerConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+/*import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -13,15 +13,15 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;*/
 
 import java.util.Arrays;
 
 @Configuration
-@EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+/*@EnableAuthorizationServer*/
+public class AuthorizationServerConfig /*extends AuthorizationServerConfigurerAdapter*/ {
 
-    @Autowired
+  /*  @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
@@ -72,6 +72,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         jwtAccessTokenConverter.setSigningKey(JwtConfig.RSA_PRIVADA);
         jwtAccessTokenConverter.setVerifierKey( JwtConfig.RSA_PUBLICA);
          return jwtAccessTokenConverter;
-    }
+    }*/
 
 }
